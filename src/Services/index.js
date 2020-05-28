7d726b42eb78281a3d21b9bd86fc159fdce83bde
Damon McMahon <damon.mcmahon@studytafensw.edu.au>
2020-05-28 16:17:39 +1000

    commit to branch

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,5 +1,7 @@
 const PrisonerService = require("./PrisonerService");
+const GuardService = require("./GuardService");
 
 module.exports = {
-    PrisonerService
-}
+    PrisonerService,
+    GuardService
+};
